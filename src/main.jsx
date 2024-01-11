e455c743b25f2434c103f8e67274ef0b7c3f3cba
YeetPizza74 <68444559+YeetPizza74@users.noreply.github.com>
2024-01-11 19:47:00 +0000

    setup

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+let sqlite3 = require('sqlite3')
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
