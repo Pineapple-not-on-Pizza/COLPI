a811db657eb82c967d1733e6a6a34d7d20b3e06a
YeetPizza74 <68444559+YeetPizza74@users.noreply.github.com>
2024-01-14 01:27:56 +0000

    json?

---
+++ temp.py
@@ -0,0 +1,24 @@
+import csv
+import json
+
+file = open("t.csv", encoding="utf-8")
+
+db = {}
+
+csvreader = csv.reader(file)
+
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+for i, row in enumerate(csvreader):
+    if (row[1][0] in alphabet) & (row[1][1] in alphabet):
+        try:
+            db[row[1]].append(row[0])
+        except:
+            db[row[1]] = []
+            db[row[1]].append(row[0])
+
+print(db)
+
+save_file = open("test.json", "w")
+json.dump(db, save_file, indent = 6)  
+save_file.close()
