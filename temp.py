f310e4203f49b86dda73eebd14edb623f74027ec
MiniNoller <103951735+MiniNoller@users.noreply.github.com>
2024-01-14 16:36:50 +0100

    .

--- temp.py
+++ temp.py
@@ -1,21 +1,18 @@
 import csv
 import json
 
-file = open("t.csv", encoding="utf-8")
+file = open("./t.csv", encoding="utf-8")
 
 db = {}
 
 csvreader = csv.reader(file)
 
-alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
 for i, row in enumerate(csvreader):
-    if (row[1][0] in alphabet) & (row[1][1] in alphabet):
-        try:
-            db[row[1]].append(row[0])
-        except:
-            db[row[1]] = []
-            db[row[1]].append(row[0])
+    try:
+        db[row[1]].append(row[0])
+    except:
+        db[row[1]] = []
+        db[row[1]].append(row[0])
 
 print(db)
 
